9e700e65e0564b3a2eca19e2e5c897ef62883d89
Ekim <113715365+ekimlcn@users.noreply.github.com>
2024-08-11 10:22:44 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -5,6 +5,10 @@ public class Calculator {
         return a + b;
     }
 
+    public int extraction(int a, int b) {
+        return a - b;
+    }
+    //SJKQJNWJKLFBAWELHKGEBABWHEK
     public double addition(double a, double b) {
         return b + a;
     }
